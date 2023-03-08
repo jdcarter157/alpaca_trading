f7fa0463f3f1fb34ef8d17e87cc5699c2877cf81
Jordan <85311538+jdcarter157@users.noreply.github.com>
2023-03-08 11:53:47 -0500

    overall improvements to everything

--- alp_scalp.py
+++ alp_scalp.py
@@ -1,8 +1,10 @@
 import alpaca_trade_api as tradeapi
+from alpaca_trade_api.stream import Stream
 
 import pandas as pd
 import time
 import requests
+
 # Define your Alpaca API credentials
 api_key = ''
 api_secret = ''
@@ -10,11 +12,10 @@ base_url = 'https://paper-api.alpaca.markets'
 
 # Authenticate your client
 api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+
 # Define the symbol and timeframe for the data
 symbol = 'VOO'
 timeframe = '1Min'
-response = requests.get(base_url)
-
 
 # Check if we can connect to the API by getting the account information
 account_info = api.get_account()
@@ -25,81 +26,97 @@ asset_info = api.get_asset(symbol)
 print("Asset Info for {}: ".format(symbol))
 print(asset_info)
 
-while True:
-    # Load the data from Alpaca
-    data = api.get_bars(symbol,timeframe,limit=288).df
-    # print(data)
-    time.sleep(1)  # Pause for 1 second between requests to avoid rate limiting
 
-    # Calculate the moving averages
-    ma_20 = data['close'].rolling(window=20).mean()
-    ma_50 = data['close'].rolling(window=50).mean()
+# Define the trading strategy function
+def trading_strategy(indicators):
+    print("entered trading strategy:")
+    try:
+        # If the close price is above the upper Bollinger Band and the 20-day moving average is above the 50-day moving average, sell the security
+        if indicators['price'] > indicators['Upper_Band'] and indicators['MA_20'] > indicators['MA_50']:
+            return 'SELL'
+        # If the close price is below the lower Bollinger Band and the 20-day moving average is below the 50-day moving average, buy the security
+        elif indicators['price'] < indicators['Lower_Band'] and indicators['MA_20'] < indicators['MA_50']:
+            return 'BUY'
+        else:
+            return 'HOLD'
+    except Exception as e:
+        print(e)
+        return 'HOLD'
 
-    # Calculate the Bollinger Bands
-    std_20 = data['close'].rolling(window=20).std()
+
+# Define the order function
+def place_order(signal, product_id=symbol):
+    print("entered place order:")
+    if signal == 'BUY':
+        # Place a buy order for 1 unit of the product
+        api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='buy',
+            type='market',
+            time_in_force='gtc',
+        )
+        print(f"Placed a buy order for {product_id}")
+
+    elif signal == 'SELL':
+        # Place a sell order for 1 unit of the product
+        api.submit_order(
+            symbol=symbol,
+            qty=1,
+            side='sell',
+            type='market',
+            time_in_force='gtc',
+        )
+        print(f"Placed a sell order for {product_id}")
+    elif signal == 'HOLD':
+
+        print(f"HOLD or WAIT for better opportunities on {product_id}")
+
+
+# Create an empty dataframe to store the quotes
+data = pd.DataFrame(columns=['symbol', 'bid_exchange', 'bid_price', 'bid_size', 'ask_exchange', 'ask_price', 'ask_size', 'conditions', 'tape', 'timestamp'])
+
+async def quote_callback(q):
+    print('quote', q)
+
+    # Convert the quote object to a dictionary
+    q_dict = q._raw
+
+
+    # Append the quote to the dataframe
+    global data
+    # data = data.append(q_dict, ignore_index=True)
+    data = pd.concat([data, pd.DataFrame.from_dict([q_dict])], ignore_index=True)
+    print('data',data)
+    # Convert the timestamp column to a datetime object
+    # data['timestamp'] = pd.to_datetime(data['timestamp'], unit='us').tz_localize('UTC').tz_convert('US/Eastern')
+
+    # Calculate the moving averages and Bollinger Bands
+    ma_20 = data['bid_price'].rolling(window=20).mean()
+    ma_50 = data['bid_price'].rolling(window=50).mean()
+    std_20 = data['bid_price'].rolling(window=20).std()
     upper_band = ma_20 + (std_20 * 1.5)
     lower_band = ma_20 - (std_20 * 1.5)
 
     # Create a new dataframe with the indicators
     indicators = pd.DataFrame()
-    indicators['Close'] = data['close'].astype(float)
+    indicators['price'] = data['bid_price'].astype(float)
     indicators['MA_20'] = ma_20.astype(float)
     indicators['MA_50'] = ma_50.astype(float)
     indicators['Upper_Band'] = upper_band
     indicators['Lower_Band'] = lower_band
-    # print(indicators)
-
-    # Define the trading strategy function
-    def trading_strategy(data):
-        # print("hey")
-        try:
-            # If the close price is above the upper Bollinger Band and the 20-day moving average is above the 50-day moving average, sell the security
-            if data['Close'] > data['Upper_Band'] and data['MA_20'] > data['MA_50']:
-                return 'SELL'
-            # If the close price is below the lower Bollinger Band and the 20-day moving average is below the 50-day moving average, buy the security
-            elif data['Close'] < data['Lower_Band'] and data['MA_20'] < data['MA_50']:
-                return 'BUY'
-            else:
-                return 'HOLD'
-        except Exception as e:
-            print(e)
-            return 'HOLD'
+    print("indicators",indicators)
+    print("length of data",len(data))
 
 
-    # Apply the trading strategy function to the indicators dataframe
-    indicators['Signal'] = indicators.apply(trading_strategy, axis=1)
-    # print("yoyo")
+    place_order(trading_strategy(indicators))
 
-    # Define the order function
-    def place_order(signal, product_id=symbol):
-        # print("yurp")
-        if signal == 'BUY':
-            # Place a buy order for 1 unit of the product
-            api.submit_order(
-                symbol=symbol,
-                qty=1,
-                side='buy',
-                type='market',
-                time_in_force='gtc',
-            )
-            print(f"Placed a buy order for {product_id}")
-
-        elif signal == 'SELL':
-            # Place a sell order for 1 unit of the product
-            api.submit_order(
-                symbol=symbol,
-                qty=1,
-                side='sell',
-                type='market',
-                time_in_force='gtc',
-            )
-            print(f"Placed a sell order for {product_id}")
-        else:
-            print('No order placed')
-
-        # Place an order based on the latest signal
-    latest_signal = indicators['Signal'].iloc[-1]
-    # print("latest signal")
-    print(latest_signal)
-    place_order(latest_signal)
-
+# Initiate the quote stream
+stream = Stream(api_key, api_secret, base_url="wss://stream.data.alpaca.markets/v2/iex",
+                data_feed='iex')
+stream.subscribe_quotes(quote_callback, symbol)
+try:
+    stream.run()
+except Exception as e:
+    print(e)
+print("sup3")
